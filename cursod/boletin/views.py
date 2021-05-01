18240a0557a0ad8a70f47379ccb8239422018f5d
Jose-MRS <josemaria.riol.sanchez.alu@iesfernandoaguilar.es>
2021-05-01 19:35:42 +0200

    Continuacion

--- cursod/boletin/views.py
+++ cursod/boletin/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 from .forms import RegForm
 # Create your views here.
 def inicio(request):
-    form = RegForm()
+    form = RegForm(request.POST or None)
+    if form.is_valid():
+        form_data = form.cleaned_data
+        print(form_data.get("nombre"))
+        print(form_data.get("edad"))
     context = {"el_form":form,}
     return render(request, "inicio.html", context)
