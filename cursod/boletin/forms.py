82c38a86b190613bad889301adace1fcc3e0f4bf
Jose-MRS <josemaria.riol.sanchez.alu@iesfernandoaguilar.es>
2021-05-01 20:36:01 +0200

    Continuacion

--- cursod/boletin/forms.py
+++ cursod/boletin/forms.py
@@ -6,6 +6,18 @@ class RegModelForm(forms.ModelForm):
     class Meta:
         model = Registrado
         fields = ["nombre", "email"]
+    
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        email_base, proveeder = email.split("@")
+        dominio, extension = proveeder.split(".")
+        if not extension == "edu":
+            raise forms.ValidationError("Por favor utiliza un email con la extensión .EDU")
+        return email
+    def clean_nombre(sed):
+        nombre = self.cleaned_data.get("nombre")
+        #validaciones
+        return nombre
 
 class RegForm(forms.Form):
     nombre = forms.CharField(max_length=100)
